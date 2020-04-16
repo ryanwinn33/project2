69d61088752aa8a9fa20ae8e46ce0a931b32e944
Ryan Winn <ryanwinn8@gmail.com>
2020-04-15 21:16:21 -0700

    updated choropleth w/ interactivity

--- mapping/static/js/logic.js
+++ mapping/static/js/logic.js
@@ -21,7 +21,7 @@ function getColor(d) {
            d > 10   ? '#FED976' :
                       '#FFEDA0';
 }
-// 
+
 function style(feature) {
     return {
         fillColor: getColor(feature.properties.density),
@@ -37,3 +37,86 @@ L.geoJson(statesData, {style: style}).addTo(map);
 
 // Create a function to add interactivity with mouseover
 
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+// Hover over a state to see the Total obligated cost and the Disaster Count
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Total Obligated Cost & Disaster Count</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' USD' +
+        '<b>' + '<h4>Disaster Count: </h4>' //add variable for disaster count
+        : 'Hover over a state');
+};
+
+info.addTo(map);
+
+
+
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+var geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [],
+        from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(map);
