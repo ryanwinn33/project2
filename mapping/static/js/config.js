979f8c14a25ac916301c59837ca36a0901afc030
Ryan Winn <ryanwinn8@gmail.com>
2020-04-15 18:38:31 -0700

    adding choropleth skeleton

---
+++ mapping/static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoicnlhbndpbm4wIiwiYSI6ImNrOGx0bGZ4MjBmajIzZW56bzdkMW5ya2EifQ.8sE2yFNX-GdxX2C4CisikA";
+
---
+++ mapping/static/js/logic.js
@@ -0,0 +1,39 @@
+var mapboxAccessToken = "pk.eyJ1Ijoicnlhbndpbm4wIiwiYSI6ImNrOGx0bGZ4MjBmajIzZW56bzdkMW5ya2EifQ.8sE2yFNX-GdxX2C4CisikA";
+var map = L.map('map').setView([37.8, -96], 4);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+    id: 'mapbox/light-v9',
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+L.geoJson(statesData).addTo(map);
+
+// Create a function to add color based on disaster count.
+function getColor(d) {
+    return d > 1000 ? '#800026' : //just an example using population density from us-states.js
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+// 
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+L.geoJson(statesData, {style: style}).addTo(map);
+
+// Create a function to add interactivity with mouseover
+
