ee3816af8eb1355f98582a1033bd55e8484bfbfb
Ryan Winn <ryanwinn8@gmail.com>
2020-04-20 17:38:53 -0700

    updated logic.js

--- mapping/static/js/logic.js
+++ mapping/static/js/logic.js
@@ -8,32 +8,42 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1
 }).addTo(map);
 
-L.geoJson(statesData).addTo(map);
+var geojson;
 
-// Create a function to add color based on disaster count.
-function getColor(d) {
-    return d > 1000 ? '#800026' : //just an example using population density from us-states.js
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
-}
+d3.json('http://127.0.0.1:5000/api/disasters', function(data){
 
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
+    // L.geoJson(statesData).addTo(map);
 
-L.geoJson(statesData, {style: style}).addTo(map);
+    // Create a function to add color based on disaster count.​
+    /**
+     * This will style each marker.
+     * @param {*} feature 
+     */
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.name, data),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    }
+
+    geojson = L.geoJson(statesData, {
+        style: style,
+        onEachFeature: function(feature, layer){
+           return {
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            }
+        }
+    }
+    ).addTo(map);
+
+});
+
 
 // Create a function to add interactivity with mouseover
 
@@ -47,15 +57,64 @@ info.onAdd = function (map) {
 // Hover over a state to see the Total obligated cost and the Disaster Count
 info.update = function (props) {
     this._div.innerHTML = '<h4>Total Obligated Cost & Disaster Count</h4>' +  (props ?
-        '<b>' + props.name + '</b><br />' + props.density + ' USD' +
-        '<b>' + '<h4>Disaster Count: </h4>' //add variable for disaster count
+        '<b>' + props.name + '</b><br />' + props.Total + ' USD' +
+        '<b>' + '<h4>Disaster Count: </h4>' + props.Incident_Count //add variable for disaster count
         : 'Hover over a state');
 };
 
 info.addTo(map);
 
+var year = 2016;
 
+/**
+ * 
+ * @param {*} name 
+ */
+function getColor(name, apidata) {
 
+    console.log(apidata.Result);
+
+    console.log(typeof apidata.Result);
+    console.log(`State name is: ${name}`);
+    console.log(`Its type is: ${typeof name}`)
+
+    var test = '';
+    if (typeof name === 'number') {
+        console.log("this is a number skipping value");
+        return;
+    }
+
+    //first loop through each year
+ 
+
+    // then loop through each state
+
+    var dollars = 0;
+
+    if (typeof apidata === 'object') {
+        apidata.Result.forEach(function(data){
+            if(data.State === name) {
+                console.log(`Yo I am on state ${data.State} and I found a match`)
+                if(data.Year === year){                  
+                    dollars = data.Total;
+                    console.log(`dollars be this much ${dollars}`);
+                }
+            }
+        });
+    }
+    else{
+        console.log('Apidata is empty');
+    }
+
+    return dollars === '$29,301,996.35 ' ? '#800026' : //just an example using population density from us-states.js
+        dollars > 10000000  ? '#BD0026' :
+        dollars > 5000000 ? '#E31A1C' :
+        dollars > 2000000  ? '#FC4E2A' :
+        dollars > 500000   ? '#FD8D3C' :
+        dollars > 20   ? '#FEB24C' :
+        dollars > 10   ? '#FED976' :
+                    '#FFEDA0';
+}
 
 function highlightFeature(e) {
     var layer = e.target;
@@ -74,17 +133,28 @@ function highlightFeature(e) {
     info.update(layer.feature.properties);
 }
 
-var geojson;
-
+/**
+ * 
+ * @param {*} e 
+ */
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
     info.update();
 }
 
+/**
+ * 
+ * @param {*} e 
+ */
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
 
+/**
+ * This function adds an event per feature from our dataset. 
+ * @param {LeafletObject} feature This is the feature that leaflet will add per gps coordinate.
+ * @param {*} layer 
+ */
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
@@ -92,31 +162,32 @@ function onEachFeature(feature, layer) {
         click: zoomToFeature
     });
 }
-geojson = L.geoJson(statesData, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
 
-var legend = L.control({position: 'bottomright'});
+// geojson = L.geoJson(statesData, {
+//     style: style,
+//     onEachFeature: onEachFeature
+// }).addTo(map);
 
-legend.onAdd = function (map) {
+// var legend = L.control({position: 'bottomright'});
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [],
-        from, to;
+// legend.onAdd = function (map) {
 
-    for (var i = 0; i < grades.length; i++) {
-        from = grades[i];
-        to = grades[i + 1];
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+//         labels = [],
+//         from, to;
 
-        labels.push(
-            '<i style="background:' + getColor(from + 1) + '"></i> ' +
-            from + (to ? '&ndash;' + to : '+'));
-    }
+//     for (var i = 0; i < grades.length; i++) {
+//         from = grades[i];
+//         to = grades[i + 1];
 
-    div.innerHTML = labels.join('<br>');
-    return div;
-};
+//         labels.push(
+//             '<i style="background:' + getColor(from + 1) + '"></i> ' +
+//             from + (to ? '&ndash;' + to : '+'));
+//     }
 
-legend.addTo(map);
+//     div.innerHTML = labels.join('<br>');
+//     return div;
+// };
+
+// legend.addTo(map);
